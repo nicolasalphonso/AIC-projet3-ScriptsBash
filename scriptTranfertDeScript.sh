af85668b32631e40101285a41dff79bc36ab26a0
root <root@debiandhcpsrv.alphonso.fr>
2021-05-23 23:26:05 +0200

    Amélioration des commentaires

--- scriptTranfertDeScript.sh
+++ scriptTranfertDeScript.sh
@@ -7,21 +7,21 @@
 # argument : liste des IP des machines où il faut transférer le fichier
 transfert_fichier()
 {
+#connection à chaque machine de la liste
 for adresse in $1
 	do
 		echo "Transfert au PC : $adresse"
-		sshpass -p administrateur scp -p /root/scripts/scriptInitialisation.sh administrateur@$adresse:/home/administrateur/scriptInitialisation.sh
+		# connection au PC distant sans prompt
+		echo "yes" | sshpass -p administrateur scp -p /root/scripts/scriptInitialisation.sh administrateur@$adresse:/home/administrateur/scriptInitialisation.sh
 	done
 }
 
-adresseFichierTransfert=""
-
 #création de la liste des ip des ordinateurs connectés
 adressesIpAbeille=$(fping -agq 192.168.100.2 192.168.100.254)
-adressesIpBaobab=$(fping -agq 192.168.101.2 192.168.101.254)
+echo "Adresses actives du réseau abeille: $adressesIpAbeille"
 
-echo "Adresses du réseau abeille actives: $adressesIpAbeille"
-echo "Adresses du réseau baobab actives: $adressesIpBaobab"
+adressesIpBaobab=$(fping -agq 192.168.101.2 192.168.101.254)
+echo "Adresses actives du réseau baobab: $adressesIpBaobab"
 
 transfert_fichier $adressesIpAbeille
 transfert_fichier $adressesIpBaobab
