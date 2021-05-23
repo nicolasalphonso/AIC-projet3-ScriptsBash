d61c11982e90ab18822c037d1a3f46443dd31f46
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-05-23 19:20:01 +0200

    ajout de la gestion du compte administrateur

--- scriptInitialisation.sh
+++ scriptInitialisation.sh
@@ -4,13 +4,18 @@
 # test de l'initialisation du compte utilisateur et changement du hostname
 # +test de la réinitialisation de la machine
 
-## Effacement de tous les utilisateurs sauf root
+## Effacement de tous les utilisateurs sauf root et administrateur
+# on récupère la liste des noms des utilisateurs
 tableauUtilisateurs=$(grep bash /etc/passwd | cut -f1 -d:)
 
 # on enlève root du tableau d'utilisateurs
 tableauUtilisateurs=("${tableauUtilisateurs[@]/root}")
 
-# on supprimer les utilisateurs
+# on enlève administrateur du tableau d'utilisateurs
+tableauUtilisateurs=("${tableauUtilisateurs[@]/administrateur}")
+
+# on supprime les autres utilisateurs
+# ainsi que leur espace personnel
 for utilisateur in $tableauUtilisateurs
 do
 	echo "utilisateur à effacer: $utilisateur"
@@ -18,20 +23,19 @@ do
 	rm -r /home/$utilisateur
 done
 
+## détermination du hostname
 # récupération de l'adresse ip
 adresseIp=$(hostname -I)
 echo "Adresse Ip du PC : $adresseIp"
 
 # détermination du numéro de la machine en gardant
-# je garde la partie hôte de l'adresse ip
+# la partie hôte de l'adresse ip sur 3 chiffres
 numeroHostname=$(echo $adresseIp | cut -c13-)
 numeroHostname=$(printf %03d $numeroHostname)
 echo "le numéro d'hôte est : $numeroHostname"
 
 # détermination du réseau auquel appartient le PC
-# cela donne le début du nom du hostname
-
-
+# cela donne le début du nom du hostname = abeille ou baobab
 debutNomHostname=""
 
 if [[ $adresseIp == "192.168.100."* ]]
@@ -47,26 +51,29 @@ echo "Ce PC n'est connecté à aucun réseau auquel il devrait l'être"
 exit 1
 fi
 
-# concaténation du hostname choisi
+# concaténation du hostname choisi avec le numéro d'hôte
 hostnameChoisi=${debutNomHostname}${numeroHostname}
 echo "Le hostname est: $hostnameChoisi"
 
 # changement du hostname de l'ordinateur
 hostnamectl set-hostname $hostnameChoisi
 
-
-# creation de l'utilisateur
-# avec création préalable du groupe
+## création de l'utilisateur
+# création du groupe
+# s'il existe déjà, il y aura juste un message d'avertissement
 addgroup $debutNomHostname
 
+# détermination du nom d'utilisateur = u(+)réseau
 usernameChoisi=u${hostnameChoisi}
 
+# ajout de l'utilisateur avec création de son espace
 useradd --home-dir /home/${usernameChoisi} --create-home --groups ${debutNomHostname} --gid ${debutNomHostname} --shell /bin/bash ${usernameChoisi}
 
-echo -e "mdp\nmdp"
-
+# activation de l'utilisateur en validant son mot de passe "mdp"
 echo -e "mdp\nmdp" | passwd ${usernameChoisi}
 
+# le mot de passe devra être changé à la première connexion
 passwd -e ${usernameChoisi}
 
+## message de fin
 echo "Ce PC est initialisé avec l'utilisateur ${usernameChoisi}, le mot de passe initial \"mdp\". Il est connecté au réseau ${debutNomHostname}"
